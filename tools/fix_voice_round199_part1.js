dccf84fe8b2104339da423a8794ce5b229a98acf
xiafeng <xiafeng@oray.com>
2026-08-25 23:53:17 +0800

    R199 voice cleanup: 视线凝定/视线凝滞/视线钉住/视线停住/目光锁住/目光钉住/目光凝定/目光停住 -> 108 replacements, 100 ch. All sources 0. CJK 3,785,645.

---
+++ tools/fix_voice_round199_part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const NN1 = '视线凝定';
+const NN2 = '视线凝滞';
+const NN3 = '视线钉住';
+const NN4 = '视线停住';
+const NN5 = '目光锁住';
+const NN6 = '目光钉住';
+const NN7 = '目光凝定';
+const NN8 = '目光停住';
+
+const VOLUMES = ['volume-1','volume-2','volume-3','volume-4','volume-5','volume-6','volume-7'];
+const TARGET = 3020;
