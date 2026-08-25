69c6d022f4191c02fbb2df942f95b3ec16a29d8b
xiafeng <xiafeng@oray.com>
2026-08-25 20:19:44 +0800

    R156 voice cleanup: 180 replacements (163 ch). All sources -> 0. CJK 3,785,634 (+66).

---
+++ tools/fix_voice_round156_part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const NN1 = '目光凝滞';
+const NN2 = '眼定住';
+const NN3 = '眼凝定';
+const NN4 = '目光停定';
+const NN5 = '声调间';
+const NN6 = '声调里';
+const NN7 = '目光定住';
+const NN8 = '声调之间';
+
+const VOLUMES = ['volume-1','volume-2','volume-3','volume-4','volume-5','volume-6','volume-7'];
+const TARGET = 3020;
