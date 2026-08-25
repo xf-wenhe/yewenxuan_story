d1df8252fd6045f49fe96ec3ec4c40005db86da4
xiafeng <xiafeng@oray.com>
2026-08-26 00:11:28 +0800

    R202 voice cleanup: 80 replacements (视线凝住/定着/凝滞, 目光停住/不转, 声调未动, 视线定住, 目光定住), 78 ch, all -> 0, CJK 3,785,645 (+0)

---
+++ tools/fix_voice_round202_part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const NN1 = '视线凝住';
+const NN2 = '视线定着';
+const NN3 = '视线凝滞';
+const NN4 = '目光停住';
+const NN5 = '目光不转';
+const NN6 = '声调未动';
+const NN7 = '视线定住';
+const NN8 = '目光定住';
+
+const VOLUMES = ['volume-1','volume-2','volume-3','volume-4','volume-5','volume-6','volume-7'];
+const TARGET = 3020;
