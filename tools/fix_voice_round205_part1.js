cb46d248f9cf801061b1e52e0185d5de0cf5723e
xiafeng <xiafeng@oray.com>
2026-08-26 00:26:10 +0800

    R205 voice cleanup: 118 replacements (视线凝住/定住/收住, 目光定住/凝定/停住, 声调未移/没变), 106 ch, all -> 0, CJK 3,785,645 (+0)

---
+++ tools/fix_voice_round205_part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const NN1 = '视线凝住';
+const NN2 = '目光定住';
+const NN3 = '视线定住';
+const NN4 = '视线收住';
+const NN5 = '目光凝定';
+const NN6 = '声调未移';
+const NN7 = '声调没变';
+const NN8 = '目光停住';
+
+const VOLUMES = ['volume-1','volume-2','volume-3','volume-4','volume-5','volume-6','volume-7'];
+const TARGET = 3020;
